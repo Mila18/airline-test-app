020d3fd1cab3c918492e176b35b311e4b7cba29c
Mila18 <milana.ab@mail.ru>
2022-06-22 22:54:29 +0500

    seats schema

--- src/pages/Seat.js
+++ src/pages/Seat.js
@@ -4,61 +4,33 @@ function Seat(){
         <div className="container-xl my-5">
             <div className="row">
                 <div className="col-6">
-                    <h3>Условные обозначения</h3>
+                    <h3 className="mb-5">Условные обозначения</h3>
+                    <label htmlFor="squareC">Свободное место</label>
+                    <div className="square mb-4" id="squareC"></div>
+                    <label htmlFor="squareB">Занято</label>
+                    <div className="squareB mb-4" id="squareB"></div>
+                    <label htmlFor="squareU">Выбранное место</label>
+                    <div className="squareChecked mb-4" id="squareU"></div>
+                    
+
                 </div>
                 <div className="col-6">
-                    {/* <div className="row">
+                <div className="row">
                         <div className="col-2 text-center">
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
+                        <h3>A</h3>
                         </div>
                         <div className="col-2 text-center">
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
+                        <h3>B</h3>
                         </div>
                         <div className="col-2 text-center">
-                            <h3 className="mb-3">1</h3>
-                            <h3 className="mb-3">2</h3>
-                            <h3 className="mb-3">3</h3>
-                            <h3 className="mb-3">4</h3>
-                            <h3 className="mb-3">5</h3>
-                            <h3 className="mb-3">6</h3>
-                            <h3 className="mb-3">7</h3>
-                            <h3 className="mb-3">8</h3>
                         </div>
                         <div className="col-2 text-center">
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
+                        <h3>C</h3>
                         </div>
                         <div className="col-2 text-center">
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
-                            <input type="checkbox"/>
+                        <h3>D</h3>
                         </div>
-                    </div> */}
+                    </div>
                     <div className="row">
                         <div className="col-2 text-center">
                             <input type="checkbox"/>
@@ -81,7 +53,7 @@ function Seat(){
                             <input type="checkbox"/>
                         </div>
                         <div className="col-2 text-center">
-                            <input type="checkbox"/>
+                            <input type="checkbox" className="occupied"/>
                         </div>
                         <div className="col-2 text-center">
                             <h3>2</h3>
